ca7c1b24090fe68dc2902e6bafa853c3fed399a2
fkhan <fkhan@fortecmedical.com>
2026-09-18 17:02:53 +0530

    Refactor import statements for consistency and clarity

--- src/py_rest_api/resources/user.py
+++ src/py_rest_api/resources/user.py
@@ -1,4 +1,3 @@
-from flask import abort
 from flask.views import MethodView
 from flask_jwt_extended import (
     create_access_token,
@@ -7,7 +6,7 @@ from flask_jwt_extended import (
     get_jwt_identity,
     jwt_required,
 )
-from flask_smorest import Blueprint
+from flask_smorest import Blueprint, abort
 from sqlalchemy.exc import IntegrityError, SQLAlchemyError
 from werkzeug.security import check_password_hash, generate_password_hash
 
